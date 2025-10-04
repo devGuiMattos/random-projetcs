7c091658994921dc1b1dfb2c402d18a728eb74d6
devGuiMattos <guidemattosmarques@gmail.com>
2025-10-04 16:47:30 -0300

    Add files via upload
    First commit

---
+++ script.py
@@ -0,0 +1,53 @@
+import math
+import random
+
+class System:
+    def out():
+        message = input("What message?\n")
+        print(". . .")
+        print(message)
+    
+    def calc():
+        calc_type = input("What operation? + - * /\n")
+        values = int(input("Value 1: \n")), int(input("Value 2: \n"))
+        if calc_type == "+":
+            print(values[0] + values[1])
+        elif calc_type == "-":
+            print(values[0] - values[1])
+        elif calc_type == "*":
+            print(values[0] * values[1])
+        elif calc_type == "/":
+            print(values[0] / values[1])
+
+    def getRandom():
+        range = int(input("Minimun number: ")), int(input("Maximum number: "))
+        print(random.randint(range[0], range[1]))
+
+    def piramid():
+        start = 1
+        amount = int(input("Enter the amount of lines:\n"))
+        print(" - - - \n")
+        while start <= amount:
+            print("1" * start)
+            start = start + 1
+            
+
+    def reverse_piramid():
+        amount = int(input("Enter amount of lines:\n"))
+        print(" - - - \n")
+        while amount >= 1:
+            print("1" * amount)
+            amount = amount - 1
+
+    def full_piramid():
+        start = 1
+        amount = int(input("Enter amount of lines (Remember that this function is doubled):\n"))
+        print(" - - - ")
+        while start <= amount:
+            print("1" * start)
+            start = start + 1
+        while amount >= 1:
+            print("1" * amount)
+            amount = amount - 1
+
+System.full_piramid()
